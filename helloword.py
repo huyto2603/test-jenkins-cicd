daf46532f9a330e94b31b97d385b095481292e82
huyto2603 <root@localhost.localdomain>
2022-12-19 02:31:41 -0800

    msteam

--- helloword.py
+++ helloword.py
@@ -1,3 +1,10 @@
-print("hello word")
-print("123")
-print("eqweqwew")
+import pymsteams
+url="https://thctechgroup.webhook.office.com/webhookb2/bb16601c-e361-4dd2-a765-d3e91813f61f@c721557d-32d7-4cbd-9aad-5ba096cd7683/IncomingWebhook/ef99037f9de046f58d328001a8659a8d/183db949-d3e9-4471-85a6-381466ccba6d"
+# You must create the connectorcard object with the Microsoft Webhook URL
+myTeamsMessage = pymsteams.connectorcard(url)
+
+# Add text to the message.
+myTeamsMessage.text("this is my text")
+
+# send the message.
+myTeamsMessage.send()
